04ebc82603933aefab3028ed951140e337f463fc
abdulwahed-mansour <abdulwahed.mansour@gmail.com.com>
2021-11-03 17:37:04 +0200

    update gitignore

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const feedDisplay = document.querySelector('#feed')
+const feedDisplay = document.querySelector("#feed");
 
-fetch('http://localhost:8001/results')
-    .then(response => {return response.json()})
-    .then(data => {
-        data.forEach(article => {
-            const articleItem = `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`
-            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
-        })
-    })
-    .catch(err => console.log(err))
-
-
+fetch("http://localhost:8001/results")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((article) => {
+      const articleItem =
+        `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`;
+      feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+    });
+  })
+  .catch((err) => console.log(err));
